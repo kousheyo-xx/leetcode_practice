0c5997bf66e8ae07bd3ef7663da9446bdc994598
Kousheyo Banerjee <banerjeekush04@gmail.com>
2026-09-17 20:15:30 +0530

    [3] [Time Beats: 52.35%] [Memory Beats: 61.38%] - LeetPush

---
+++ 3-Longest-Substring-Without-Repeating-Characters.py
@@ -0,0 +1,13 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        seen=set()
+        left=0
+        ans=0
+        n=len(s)
+        for right in range(n):
+            while s[right] in seen:
+                seen.remove(s[left])
+                left+=1
+            seen.add(s[right])
+            ans=max(ans,right-left+1)
+        return ans
