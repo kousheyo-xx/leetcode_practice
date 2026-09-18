6d55e2cb9efe28190dcf38ed6352b43d2a2243c7
Kousheyo Banerjee <banerjeekush04@gmail.com>
2026-09-18 20:16:03 +0530

    [560] [Time Beats: 58.17%] [Memory Beats: 84.87%] - LeetPush

--- 560-Subarray-Sum-Equals-K.py
+++ 560-Subarray-Sum-Equals-K.py
@@ -1,9 +1,9 @@
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
-        n=len(nums)
-        count=0
         preSum={0:1}
+        n=len(nums)
         currSum=0
+        count=0
         for i in range(n):
             currSum+=nums[i]
             if currSum-k in preSum:
